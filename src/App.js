77de51de4eb2c0706d55f40db9a178a149919390
Gurdeep123singh <yanio2617275@gmail.com>
2022-07-04 21:43:11 +0530

    update our app with shop page, collection-preview and collection-item component

--- src/App.js
+++ src/App.js
@@ -2,11 +2,19 @@ import React from 'react';
 import './App.css';
 
 import Homepage from './pages/homepage/homepage.component';
+import ShopPage from './pages/shop/shop.component';
+
+import { BrowserRouter as Router, Route, Routes}  from 'react-router-dom';
 function App() {
   return (
+    <Router>
     <div >
-      <Homepage/>
+      <Routes>
+        <Route  path='/' element={<Homepage/>}/>
+        <Route  path='/shop' element={<ShopPage/>}/>
+      </Routes>
     </div>
+    </Router>
   );
 }
 
